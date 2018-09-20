9a5bb005161fd65bb8f03ea86dd29bb031f207fa
Arthur <arthur@corenzan.com>
2018-09-20 16:21:45 -0300

    Improve commentary

--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 
+# Require privilege (a.k.a. sudo)
 if test $(id -u) -ne 0; then
 	echo "🚫 Try again with root or sudo." >&2
 	exit 1
@@ -35,16 +36,16 @@ printf "
 
 "
 
-# Confirm before continueing.
+# Confirm before continueing
 read -rsp "🚦 Press ENTER to continue or CTRL-C to abort..." _
 
-# Enable debug with --debug.
+# Enable debug with --debug
 test "$1" = "--debug" && set -x
 
-# Halt on error and undeclared variables.
 set -ue
+# Halt on error or undeclared variables
 
-# Make it non interactive.
+# Flag it as non interactive
 DEBIAN_FRONTEND=noninteractive
 
 # Generate a random string.
@@ -57,16 +58,16 @@ random() {
 log_file=provision-$(date +%s).log
 exec > >(tee $log_file) 2>&1
 
-# Add Docker repository to the source list.
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu zesty stable" >> /etc/apt/sources.list.d/docker.list
+# Add Docker repository to the source list
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-# Refresh repositories and upgrade packages.
 apt-get update
 apt-get upgrade -y
 apt-get autoremove -y
+# Refresh repositories and upgrade packages
 
-# Setup encoding.
+# Setup environment encoding
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 update-locale LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
@@ -82,61 +83,61 @@ EOF
 sysctl -p
 cat /proc/sys/net/ipv6/conf/all/disable_ipv6
 
-# Clear firewall rules.
+# Clear firewall rules
 iptables -F
 iptables -t nat -F
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 
-# Accept anything from/to loopback interface.
+# Accept anything from/to loopback interface
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
-# Docker client/server communication.
+# Docker client/server communication
 iptables -A INPUT -s 127.0.0.1 -p tcp --dport 2375 -j ACCEPT
 
-# Keep established or related connections.
+# Keep established or related connections
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
-# Allow TCP connections using ports for HTTP, HTTPS and SSH.
+# Allow TCP connections using ports for HTTP, HTTPS and SSH
 acceptable_ports="822 80 443"
 for port in $acceptable_ports; do
     iptables -A INPUT -p tcp --dport $port -j ACCEPT;
 done
 
-# Allow regular pings.
+# Allow regular pings
 iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
 
-# Block any other input.
+# Block any other input
 iptables -A INPUT -j DROP
 
-# Setup common software.
 apt-get install -y build-essential apt-transport-https ca-certificates curl software-properties-common git fail2ban unattended-upgrades docker-ce
+# Setup common software
 
-# Setup Dokku.
 DOKKU_TAG=v0.10.5
+# Setup Dokku
 curl -s https://raw.githubusercontent.com/dokku/dokku/$DOKKU_TAG/bootstrap.sh | bash
 
-# Clean downloaded packages.
-apt-get clean
-
-# Only save firewall configuration after installing fail2ban and Docker.
+# Only dump iptables configuration after installing fail2ban and Docker
 iptables-save > /etc/iptables.conf
 
-# Load firewall settings when network device is up.
+# Clean downloaded packages
+apt clean
+
+# Load iptables config when network device is up
 cat > /etc/network/if-up.d/iptables <<EOF
 #!/usr/bin/env bash
 iptables-restore < /etc/iptables.conf
 EOF
 chmod +x /etc/network/if-up.d/iptables
 
-# Change root's password.
+# Change root password
 password=$(random)
 chpasswd <<< "root:$password"
 echo "🔒 root:$password"
 
-# Create an administrator account.
+# Create an administrator account
 read -p "👉 Administrator username (arthur): " username
 username=${username:-arthur}
 password=$(random)
@@ -146,16 +147,16 @@ usermod -aG sudo $username
 usermod -aG docker $username
 echo "🔒 $username:$password"
 
-# Do not ask for password when sudoing.
+# Do not ask for password when sudoing
 sed -i '/^%sudo/c\%sudo\tALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers
 
-# Setup RSA key for secure SSH authorization.
+# Setup RSA key for secure SSH authorization
 read -e -p "👉 $username's public key: " public_key
 mkdir -p /home/$username/.ssh
 echo "$public_key" >> /home/$username/.ssh/authorized_keys
 chown -R $username:$username /home/$username/.ssh
 
-# Set some secure defaults for SSH.
+# Set some secure defaults for SSH
 cat > /etc/ssh/sshd_config <<EOF
 Port 822
 LoginGraceTime 20
@@ -168,7 +169,7 @@ MaxSessions 2
 EOF
 service ssh restart
 
-# Setup unattended security upgrades.
+# Setup unattended security upgrades
 cat > /etc/apt/apt.conf.d/10periodic <<EOF
 APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Download-Upgradeable-Packages "1";
@@ -176,7 +177,7 @@ APT::Periodic::AutocleanInterval "7";
 APT::Periodic::Unattended-Upgrade "1";
 EOF
 
-# Setup swap space with half the memory available.
+# Setup swap space with half the memory available
 memory=$(free -m | awk '/^Mem:/{print $2}')
 fallocate -l $((memory/2))KB /swapfile
 chmod 600 /swapfile
